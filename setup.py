ee709e24d49e179dab62d31f1066fa85174b810d
Pavel Břoušek <brousek@ics.muni.cz>
2021-12-01 14:18:56 +0100

    fix: move dependencies to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,11 @@ setup(
     url="https://github.com/CESNET/spreg-satosa-sync.git",
     description="Script to read clients attributes from Perun RPC and write them to mongoDB for SATOSA",
     packages=find_packages(),
-    install_requires=["setuptools"],
+    install_requires=[
+        "setuptools",
+        "pycryptodome==3.11.0",
+        "pymongo==3.12.1",
+        "requests==2.26.0",
+        "PyYAML==6.0",
+    ],
 )
