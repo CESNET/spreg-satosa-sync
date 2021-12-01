752881721b596097594a434b548eeced9023a38c
Pavel Břoušek <melanger@users.noreply.github.com>
2021-12-01 13:12:45 +0100

    chore(deps): add python_requires

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spreg-satosa-sync",
+    python_requires=">=3.6",
     url="https://github.com/CESNET/spreg-satosa-sync.git",
     description="Script to read clients attributes from Perun RPC and write them to mongoDB for SATOSA",
     packages=find_packages(),
